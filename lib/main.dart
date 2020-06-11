ded982fed71bbe577f110ff836775f58710a6123
yxxx101 <y.xxx101@gmail.com>
2020-06-11 21:05:53 +0900

    Add model

--- lib/main.dart
+++ lib/main.dart
@@ -1,60 +1,40 @@
-import 'package:coriander/next_page.dart';
-import 'package:flutter/cupertino.dart';
+import 'package:coriander/main_model.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.red,
-      ),
-      home: MyHomePage(title: 'Flutter Demonstration Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  final myController = TextEditingController();
-
-  String name;
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.blue.shade900,
-        title: Text('Flutter大学'),
-        actions: <Widget>[],
-      ),
-      body: Container(
-        width: double.infinity,
-        child: ListView(
-          scrollDirection: Axis.vertical,
-          children: <Widget>[
-            ListTile(
-              title: Text('tete'),
-            ),
-            ListTile(
-              title: Text('tete'),
-            ),
-            ListTile(
-              title: Text('tete'),
-            ),
-          ],
+      home: ChangeNotifierProvider<MainModel>(
+        create: (_) => MainModel(),
+        child: Scaffold(
+          appBar: AppBar(
+            title: Text('コリアンダー'),
+          ),
+          body: Consumer<MainModel>(builder: (context, model, child) {
+            return Center(
+              child: Column(
+                children: <Widget>[
+                  Text(
+                    model.kboyText,
+                    style: TextStyle(
+                      fontSize: 30,
+                    ),
+                  ),
+                  RaisedButton(
+                    child: Text('ボタン'),
+                    onPressed: () {
+                      model.changeKboyText();
+                    },
+                  )
+                ],
+              ),
+            );
+          }),
         ),
       ),
     );
